916cd8558babecf34ed38c4f76c6bbc67fca94f6
m2tkl <kenchalros@gmail.com>
2021-09-06 01:24:24 +0900

    update login form

--- pages/login.vue
+++ pages/login.vue
@@ -1,22 +1,35 @@
 <template>
   <v-row justify="center" align-content="center">
     <v-col cols="12" sm="6">
-      <v-card flat outlined tile>
+      <v-card flat outlined tile class="pa-4">
         <v-card-title class="justify-center"> Login </v-card-title>
         <v-card-text>
-          <v-text-field v-model="email" label="user name"> </v-text-field>
+          <v-text-field v-model="email" label="Username"> </v-text-field>
           <v-text-field
+            label="Password"
             v-model="password"
-            label="password"
             :type="showPassword ? 'text' : 'password'"
             :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
             v-on:click:append="showPassword = !showPassword"
+            required
           >
           </v-text-field>
         </v-card-text>
         <v-card-actions>
           <v-btn block tile color="primary">Login</v-btn>
         </v-card-actions>
+        <v-card-text class="pl-2">
+          <ul>
+            <li>
+              Forgot
+              <nuxt-link to="/password-reset">Username / Password?</nuxt-link>
+            </li>
+            <li>
+              Don't have any account?
+              <nuxt-link to="/signup">Sign up</nuxt-link>
+            </li>
+          </ul>
+        </v-card-text>
       </v-card>
     </v-col>
   </v-row>
@@ -29,11 +42,21 @@ export default defineComponent({
   layout: 'entry',
   setup() {
     const email = ref('')
+    const password = ref('')
     const showPassword = ref(false)
+    const checkbox = ref(false)
     return {
       email,
+      password,
       showPassword,
+      checkbox,
     }
   },
 })
 </script>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+</style>
