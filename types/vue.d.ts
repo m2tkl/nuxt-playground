283e9cbab1f0c3806e246debfbf3bf3613a550c2
m2tkl <kenchalros@gmail.com>
2021-10-21 06:13:29 +0900

    define plugin type

--- types/vue.d.ts
+++ types/vue.d.ts
@@ -2,9 +2,16 @@ import {
   Repositories,
 } from "~/composables/factories/apiRepositoryFactory";
 
+import {
+  Process
+} from "~/plugins/process";
+
 
 declare module '@nuxt/types' {
   interface NuxtAppOptions {
     readonly $repositories: <K extends keyof Repositories>(key: K) => ReturnType<Repositories[K]>
+  }
+  interface NuxtAppOptions {
+    readonly $process: Process
   }
 }
